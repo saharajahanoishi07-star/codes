5e687ea4cc25bd81742ebb6676dc67adc03c6a3e
saharajahanoishi07-star <saharajahanoishi07@gmail.com>
2026-09-16 10:19:52 +0600

    Create TriangleArea.c

---
+++ TriangleArea.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Enter Base and Height: ");
+    float h,b;
+    scanf("%f %f",&b,&h);
+
+    printf("Triangle area: %f\n",0.5*b*h);    
+
+    return 0;
+}
