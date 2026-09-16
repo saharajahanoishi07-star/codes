0b1509abeb0132011961d2514c7c9c058840330e
saharajahanoishi07-star <saharajahanoishi07@gmail.com>
2026-09-16 10:30:25 +0600

    Create Oishi.c

---
+++ Oishi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char name[50], dep[50], university[100];
+    printf("Enter your name: ");
+    scanf("%s",name);
+
+    printf("Enter your department name: ");
+    scanf("%s",dep);
+
+    printf("Enter your university name: ");
+    scanf("%s",university);
+
+    printf("My name is %s, my department name is %s and my univesity name is %s university\n",name,dep,university);
+    
+    return 0;
+}
