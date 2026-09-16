527419e6b40195540026f28298ff25160228f1c0
saharajahanoishi07-star <saharajahanoishi07@gmail.com>
2026-09-16 12:54:52 +0600

    Create rectangleareasjo.c

---
+++ rectangleareasjo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    float l,w;
+    
+    printf("Enter lenght :");
+    scanf("%f",&w);
+    printf("Enter width :");
+    scanf("%f",&w);
+    printf("Rectangle Area :%f\n ",l*w);
+    
+    
+    return 0;
+}
