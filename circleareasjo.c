66856613c7b10ea7540bce0e9ce23973a35005aa
saharajahanoishi07-star <saharajahanoishi07@gmail.com>
2026-09-16 12:47:05 +0600

    Create circleareasjo.c

---
+++ circleareasjo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    
+    float a,Pi=3.14159;
+    printf("Radius :");
+    scanf("%f",&a);
+    printf("Circle Area :%f\n ",Pi*a*a);
+    
+    
+    return 0;
+}
