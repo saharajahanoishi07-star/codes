495060b08f03cac471fae58b46fac5d019797e09
saharajahanoishi07-star <saharajahanoishi07@gmail.com>
2026-09-22 21:28:34 +0600

    Create loop0-50&81-101.c

---
+++ loop0-50&81-101.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+        for(int i=0,j=81;i<=50 && j<=101;i=i+5,j=j+2){
+           printf("%d %d\n",i,j);
+             
+           } 
+         
+        return 0;
+} 
