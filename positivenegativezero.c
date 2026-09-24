d318e4e12ea285d2abd1b234c392325d9862b029
saharajahanoishi07-star <saharajahanoishi07@gmail.com>
2026-09-24 19:55:33 +0600

    Create positivenegativezero.c

---
+++ positivenegativezero.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+    scanf("%d",&n);
+    if(n>0){
+        printf("Positive Number");
+    }
+    else if(n<0){
+        printf("Negative Number");
+    }
+    else if(n==0){
+        printf("Zero");
+    }
+        return 0;
+}0
