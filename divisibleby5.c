191e3483f2c8f103fd25aa134a92219abb304b0e
saharajahanoishi07-star <saharajahanoishi07@gmail.com>
2026-09-24 20:02:36 +0600

    Create divisibleby5.c

---
+++ divisibleby5.c
@@ -0,0 +1,16 @@
+// Online C compiler (editor)
+// Write and run C online using this editor.
+
+#include <stdio.h>
+
+int main() {
+  int n;
+    scanf("%d",&n);
+    if(n%5==0){
+        printf("Divisible by 5");
+    }
+    else{ 
+        printf("Not divisible");
+    }
+        return 0;
+}
