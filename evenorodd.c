0c2b472ccc110696efd2dbe9375fbda371cd4e3d
saharajahanoishi07-star <saharajahanoishi07@gmail.com>
2026-09-24 19:59:45 +0600

    Create evenorodd.c

---
+++ evenorodd.c
@@ -0,0 +1,16 @@
+// Online C compiler (editor)
+// Write and run C online using this editor.
+
+#include <stdio.h>
+
+int main() {
+  int n;
+    scanf("%d",&n);
+    if(n%2==0){
+        printf("even");
+    }
+    else if(n%2!=0){
+        printf("odd");
+    }
+        return 0;
+}
