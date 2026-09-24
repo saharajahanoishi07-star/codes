108ae861fab5eb8b618d009e4144bd11d3ce3304
saharajahanoishi07-star <saharajahanoishi07@gmail.com>
2026-09-24 20:16:32 +0600

    Create 32vote.c

---
+++ 32vote.c
@@ -0,0 +1,18 @@
+// Online C compiler (editor)
+// Write and run C online using this editor.
+
+#include <stdio.h>
+
+int main()  { 
+    printf("Enter marks :");
+  int m;
+    scanf("%d",&m);
+    if(m>=18){
+        printf("Elligible for vote");
+    }
+    else{ 
+        printf("Not elligible");
+    }
+        return 0;
+}
+    
