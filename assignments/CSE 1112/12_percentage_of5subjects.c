afc6ce814d0117b7b6c423b41ca8f4d9b9f1dcf6
saharajahanoishi07-star <saharajahanoishi07@gmail.com>
2026-09-25 15:55:58 +0600

    Create 12_percentage_of5subjects.c

---
+++ assignments/CSE 1112/12_percentage_of5subjects.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+ float a,b,c,d,f;
+    printf("5 subjects marks :");
+    scanf("%f%f%f%f%f",&a,&b,&c,&d,&f);
+   printf("Percentageo of 5 subjects :%f",((a+b+c+d+f)/500)*100);
+    
+    
+    return 0;
+}
