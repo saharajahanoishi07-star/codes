90eaf1ac2a800b7cccdd8654f84dd02c272d30ec
saharajahanoishi07-star <saharajahanoishi07@gmail.com>
2026-09-25 16:19:01 +0600

    Create 18_simple_interest.c

---
+++ assignments/CSE 1112/18_simple_interest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+ float a,b,c;
+    printf("principal,Rate of interest,Time : ");
+    scanf("%f%f%f",&a,&b,&c);
+    printf("simple interest:%f",(a*b*c)/100);
+           
+    
+    
+    return 0;
+}
