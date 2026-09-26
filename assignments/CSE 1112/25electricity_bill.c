e21927900d7e6a336113594dd6fa364b99a48e4a
saharajahanoishi07-star <saharajahanoishi07@gmail.com>
2026-09-26 21:19:44 +0600

    Create 25electricity_bill.c

---
+++ assignments/CSE 1112/25electricity_bill.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int units;
+    float bill;
+
+    printf("Enter electricity units consumed: ");
+    scanf("%d", &units);
+
+    if (units <= 50)
+        bill = units * 0.50;
+    else if (units <= 150)
+        bill = 50 * 0.50 + (units - 50) * 0.75;
+    else if (units <= 250)
+        bill = 50 * 0.50 + 100 * 0.75 + (units - 150) * 1.20;
+    else
+        bill = 50 * 0.50 + 100 * 0.75 + 100 * 1.20
+               + (units - 250) * 1.50;
+
+    printf("Electricity Bill = %.2f\n", bill);
+
+    return 0;
+}
+Example
